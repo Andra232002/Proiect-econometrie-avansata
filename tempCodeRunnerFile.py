073c301a7475f61b4f0f94462d6adb5a5e358806
Andra232002 <andratobosaru2002@gmail.com>
2025-05-25 14:21:36 +0300

    Add files via upload

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,13 +1,1 @@
-# Importăm biblioteca pandas
 import pandas as pd
-
-# Citim fișierul CSV
-df = pd.read_csv("Baza_date.csv")
-
-# Afișăm primele 5 rânduri din baza de date pentru a verifica că s-a încărcat corect
-print("Primele 5 rânduri din baza de date:")
-print(df.head())
-
-# Statistici descriptive (media, min, max, etc.)
-print("\nStatistici descriptive:\n")
-print(df.describe())
